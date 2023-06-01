5ce2c98fb2e0aa99a8dc8fc80183c16bdf7fbf1d
Ilya <socspec@yandex.ru>
2023-06-01 16:44:49 +0300

    01/06/23

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -1,7 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
@@ -13,6 +13,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.import_ingredients()
+        self.import_tags()
         print('Загрузка ингредиентов завершена.')
 
     def import_ingredients(self, file='ingredients.csv'):
@@ -24,4 +25,16 @@ class Command(BaseCommand):
                 status, created = Ingredient.objects.update_or_create(
                     name=row[0],
                     measurement_unit=row[1]
+                )
+
+    def import_tags(self, file='tag.csv'):
+        print(f'Загрузка {file}...')
+        file_path = f'./data/{file}'
+        with open(file_path, newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                status, created = Tag.objects.update_or_create(
+                    name=row[0],
+                    color=row[1],
+                    slug=row[2]
                 )
