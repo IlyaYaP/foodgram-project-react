b3c2aa0e9c60a8e2fd5423b9cfea4b0b7b100184
Илья <socspec@yandex.ru>
2023-07-15 17:57:02 +0300

    1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        # 'HOST': '127.0.0.1',
+        # 'HOST': os.getenv('DB_HOST', default='db'),
+        'HOST': 'localhost',
         'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
